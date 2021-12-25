f254be5541e24397c0e2c1f19780d0c507cecf8b
Wak44 <230397s@ugs.kochi-tech.ac.jp>
2021-12-26 08:50:06 +0900

    fragmentによる画面遷移を追加 以下変更点のあるファイル MainActivity.java

--- app/src/main/java/com/example/testapp2/MainActivity.java
+++ app/src/main/java/com/example/testapp2/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
+/* activity screen transition
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +18,28 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(getApplication(), SubActivity.class);
             startActivity(intent);
         });
+    }
+}
+*/
+
+// fragment screen transition
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setScreenMain();
+    }
+    private void setScreenMain() {
+        setContentView(R.layout.activity_main);
+
+        Button sendButton = findViewById(R.id.send_button);
+        sendButton.setOnClickListener(v -> setScreenSub());
+    }
+    private void setScreenSub() {
+        setContentView(R.layout.activity_sub);
+
+        Button returnButton = findViewById(R.id.return_button);
+        returnButton.setOnClickListener(v -> setScreenMain());
     }
 }
